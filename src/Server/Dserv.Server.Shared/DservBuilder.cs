0a2bdef1808726934dd5a0a388bcface4d1a7621
Tom Gilder <tom@tom.me.uk>
2017-08-06 12:50:13 +0100

    Use shared project

--- src/Server/Dserv.Server.Shared/DservBuilder.cs
+++ src/Server/Dserv.Server.Shared/DservBuilder.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections.Generic;
 
 namespace Dserv.Server
-{   
+{
     public class DservBuilder
     {
         List<IHandler> _handlers = new List<IHandler>();
-
-        internal int Port { get; set; } = 8000;
+        int _port = 8000;
 
         public Host Build()
         {
-            return new Host(_handlers, Port);
+            return new Host(_handlers, _port);
         }
 
         public void AddHandler(IHandler handler)
         {
             _handlers.Add(handler);
+        }
+
+        public DservBuilder UsePort(int port)
+        {
+            _port = port;
+            return this;
         }
     }
 }
